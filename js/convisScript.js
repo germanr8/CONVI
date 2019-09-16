7b36d006f9641b940e2a4efb7f1ecf3de32f1737
Yair Pimentel <42229163+Yair0@users.noreply.github.com>
2019-09-15 19:14:08 -0500

    Intento JS
    Intento fallido

---
+++ js/convisScript.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+require("node_modules/bootstrap/js/*.js");
+require("node_modules/jquery/src/*.js");
---
+++ js/convisScript.ts
@@ -0,0 +1,2 @@
+import "node_modules/bootstrap/js/*.js";
+import "node_modules/jquery/src/*.js";
