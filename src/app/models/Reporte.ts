d599e3af0b7aa819e9c71458bd642603a1c470f3
Yair Pimentel <42229163+Yair0@users.noreply.github.com>
2019-10-21 15:34:32 -0500

    Resert
    Se reseteo la branch

--- src/app/models/Reporte.ts
+++
@@ -1,9 +0,0 @@
-export class Reporte {
-  constructor(
-    public titulo?: string,
-    public autor?: string,
-    public fecha?: string,
-    public direccion?: string,
-    public descripcion?: string
-  ) {}
-}
--- static_project/js/convisScript.ts
+++
@@ -1,2 +0,0 @@
-import Util from "../node_modules/bootstrap/js/src/util.js";
-import carousel from "../node_modules/bootstrap/js/src/carousel.js";
