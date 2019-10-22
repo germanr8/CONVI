7d18e3994b4620a4967f25387df8fa4cf7756614
germanr8 <german.reyga@gmail.com>
2019-10-22 16:34:39 -0500

    Sustitución de comentarios por reacciones (emojis)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,11 @@ const routes: Routes = [
     path: 'amber',
     component: AmberComponent,
     data: { animation: 'Amber' }
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/inicio'
   }
 ];
 
