9c83775053e789213fe879f14fa8c637ecc22a17
germanr8 <german.reyga@gmail.com>
2019-10-22 16:07:48 -0500

    Merge branch 'Mapa-Yair' into angular-migration

--- src/app/services/mapa.service.ts
+++ src/app/services/mapa.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@angular/core";
-import { latLng, tileLayer, circle, polygon, marker } from "leaflet";
+import { Injectable } from '@angular/core';
+import { latLng, tileLayer, circle, polygon, marker } from 'leaflet';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class MapaService {
   mapOptions;
@@ -13,20 +13,20 @@ export class MapaService {
     this.mapOptions = {
       layers: [
         tileLayer(
-          "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFpcnB2IiwiYSI6ImNrMG9jaTV0YTA5OGIzbG44YXVpcTdzNWcifQ.N79zLQXiNC_GmGttSGcIUQ",
+          'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFpcnB2IiwiYSI6ImNrMG9jaTV0YTA5OGIzbG44YXVpcTdzNWcifQ.N79zLQXiNC_GmGttSGcIUQ',
           {
             attribution:
               'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: "mapbox.streets",
+            id: 'mapbox.streets',
             accessToken:
-              "pk.eyJ1IjoieWFpcnB2IiwiYSI6ImNrMG9jaTV0YTA5OGIzbG44YXVpcTdzNWcifQ.N79zLQXiNC_GmGttSGcIUQ"
+              'pk.eyJ1IjoieWFpcnB2IiwiYSI6ImNrMG9jaTV0YTA5OGIzbG44YXVpcTdzNWcifQ.N79zLQXiNC_GmGttSGcIUQ'
           }
         )
       ],
       overlays: {},
       zoom: 10,
       center: latLng([19.35, -99.128]),
-      zoomControl: false, // Oculta los controles de zoom
+      zoomControl: true, // Oculta los controles de zoom
       dragging: false, // Evita que el usuario mueva el mapa con drag del mouse
       scrollWheelZoom: false, // Evita que el usuario mueva el mapa con el ratón del mouse
       doubleClickZoom: false, // Evita que el usuario haga zoom con doble click
@@ -44,7 +44,7 @@ export class MapaService {
   }
   getAlcaldiasDatos() {
     this.alcaldiasDatos = this.fetchJSON(
-      "../../assets/geojson/alcaldias.geojson"
+      '../../assets/geojson/alcaldias.geojson'
     );
     return this.alcaldiasDatos;
   }
