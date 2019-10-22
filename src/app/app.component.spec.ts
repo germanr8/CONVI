aef1e373421d0f5d14c2b1532fbeb6dc35c96305
germanr8 <german.reyga@gmail.com>
2019-10-22 13:45:51 -0500

    Cambio de nombre del proyecto
    Cambia de CONVIS a CONVI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'convis'`, () => {
+  it(`should have as title 'convi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('convis');
+    expect(app.title).toEqual('convi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('convis app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('convi app is running!');
   });
 });
